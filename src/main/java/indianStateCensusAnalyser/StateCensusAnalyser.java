7b8b89a3f21d88b3a28b7ed125ac3229d19d25ab
Ashok Koda <a.koda59@gmail.com>
2021-12-29 17:18:40 +0530

    StateCensusAnalyser testcases

---
+++ src/main/java/indianStateCensusAnalyser/StateCensusAnalyser.java
@@ -0,0 +1,100 @@
+package indianStateCensusAnalyser;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class StateCensusAnalyser {
+
+	private static String SAMPLE_JSON_FILE_PATH = "D:/Eclipse Java Projects/eclipse-workspace/Indian-State-Census-Analyser/CSVStatesCensusJsonFile.json";
+    private static String POPULATION_JSON_FILE_PATH = "D:/Eclipse Java Projects/eclipse-workspace/Indian-State-Census-Analyser/Population .json";
+    private static String POPULATION_DENSITY_JSON_FILE_PATH = "D:/Eclipse Java Projects/eclipse-workspace/Indian-State-Census-Analyser/PopulationDensity.json";
+    private static String LARGEST_STATE_AREA_JSON_FILE_PATH = "D:/Eclipse Java Projects/eclipse-workspace/Indian-State-Census-Analyser/LargestStateArea.json";
+    private String filePath;
+
+    public StateCensusAnalyser(String filePath) {
+        this.filePath = filePath;
+    }
+    
+    CSVStatesCensus csvStatesCensus = new CSVStatesCensus();
+    CSVStates csvStates = new CSVStates();
+    
+    public <E> int readStateData(Class<E> eClass, String sortBy) throws CensusCsvException {
+        int count = 0;
+        List<E> statesCensusList = new ArrayList<>();
+        try (Reader reader = Files.newBufferedReader(Paths.get(filePath))) {
+            CsvToBean<E> csvToBean = new CsvToBeanBuilder(reader)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .withType(eClass)
+                    .build();
+            Iterator<E> stateIterator = csvToBean.iterator();
+            while (stateIterator.hasNext()) {
+                E csv = stateIterator.next();
+                statesCensusList.add((E) csv);
+                count++;
+                if (csv instanceof CSVStatesCensus) {
+                    if (((CSVStatesCensus) csv).getState() == null || ((CSVStatesCensus) csv).getAreaInSqKm() == null || ((CSVStatesCensus) csv).getDensityPerSqKm() == null || ((CSVStatesCensus) csv).getPopulation() == null) {
+                        throw new CensusCsvException("Exception due to Header or mismatch data", CensusCsvException.ExceptionType.NO_SUCH_HEADER);
+                    }
+                }
+                if (csv instanceof CSVStates) {
+                    if (((CSVStates) csv).getSrNo() == null || ((CSVStates) csv).getStateCode() == null || ((CSVStates) csv).getStateName() == null || ((CSVStates) csv).getTIN() == null) {
+                        throw new CensusCsvException("Exception due to Header or mismatch data", CensusCsvException.ExceptionType.NO_SUCH_HEADER);
+                    }
+                }
+            }
+            sortingList((List<CSVStatesCensus>) statesCensusList, sortBy);
+        } catch (NoSuchFileException e) {
+            if (filePath.contains(".csv")) {
+                throw new CensusCsvException("Please enter proper file name", CensusCsvException.ExceptionType.NO_SUCH_FILE);
+            }
+        } catch (RuntimeException e) {
+            throw new CensusCsvException("Exception due to incorrect delimiter position", CensusCsvException.ExceptionType.NO_SUCH_FIELD);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+    
+    public void sortingList(List<CSVStatesCensus> statesCensusList, String sortType) throws IOException {
+        if (sortType.equals("stateAlphabeticalOrder")) {
+//            Comparator<CSVStatesCensus> statesCensusComparator = Comparator.comparing(CSVStatesCensus::getState);
+        	Comparator<CSVStatesCensus> statesCensusComparator = Comparator.;
+            writeToJson(statesCensusList, SAMPLE_JSON_FILE_PATH);
+        }
+        if (sortType.equals("mostPopulatedState")) {
+            Comparator<CSVStatesCensus> statesCensusComparator = Comparator.comparing(CSVStatesCensus::getPopulation);
+            statesCensusList.sort(statesCensusComparator);
+            writeToJson(statesCensusList, POPULATION_JSON_FILE_PATH);
+        }
+        if (sortType.equals("populationDensityState")) {
+            Comparator<CSVStatesCensus> statesCensusComparator = Comparator.comparing(CSVStatesCensus::getDensityPerSqKm);
+            statesCensusList.sort(statesCensusComparator);
+            writeToJson(statesCensusList, POPULATION_DENSITY_JSON_FILE_PATH);
+        }
+        if (sortType.equals("largestStateArea")) {
+            Comparator<CSVStatesCensus> statesCensusComparator = Comparator.comparing(CSVStatesCensus::getAreaInSqKm);
+            statesCensusList.sort(statesCensusComparator);
+            writeToJson(statesCensusList, LARGEST_STATE_AREA_JSON_FILE_PATH);
+        }
+    }
+    
+    public static void writeToJson(List<CSVStatesCensus> statesCensusList, String filePath) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(statesCensusList);
+        FileWriter writer = new FileWriter(filePath);
+        writer.write(json);
+        writer.close();
+    }
+}
---
+++ src/test/java/statecensusanalyser/StateCensusAnalyserTest.java
@@ -0,0 +1,164 @@
+package statecensusanalyser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import indianStateCensusAnalyser.CensusCsvException;
+import indianStateCensusAnalyser.StateCensusAnalyser;
+
+public class StateCensusAnalyserTest {
+
+	@Test
+    public void checkToEnsure_NumberOfRecordsMatches() throws CensusCsvException {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("/home/admin265/IdeaProjects/StateCensusAnalyserProblem/src/main/resources/StateCode.csv");
+    }
+
+    @Test
+    public void givenWrongFileName_ShouldThrowNoSuchFileException() {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("/home/admin265/IdeaProjects/StateCensusAnalyserProblem/src/main/resources/StateCode12.csv");
+        try {
+            int value = stateCensusAnalyser.readStateData(CSVStates.class,"");
+            Assert.assertEquals(37, value);
+        } catch (CensusCsvException e) {
+            System.out.println(e.getMessage());
+            Assert.assertEquals("Please enter proper file name", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenWrongFilePath_ShouldThrowRunTimeException() {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("/home/admin265/IdeaProjects/StateCensusAnalyserProblem/gradlew.bat");
+            int checkNumberOfRecords = stateCensusAnalyser.readStateData(CSVStates.class,"");
+        } catch (CensusCsvException e) {
+            e.printStackTrace();
+            Assert.assertEquals("Exception due to Header or mismatch data", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenCsvFile_ifFoundIncorrectDelimiterPosition_ShouldReturnException() {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("/home/admin265/IdeaProjects/StateCensusAnalyserProblem/src/main/resources/IncorrectStatecode.csv");
+            int value = stateCensusAnalyser.readStateData(CSVStates.class,"");
+            Assert.assertEquals(37, value);
+        } catch (CensusCsvException e) {
+            Assert.assertEquals("Exception due to Header or mismatch data", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenCsvFile_ifFoundNoHeader_ShouldReturnException() {
+
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("/home/admin265/IdeaProjects/StateCensusAnalyserProblem/src/main/resources/StateCode.csv");
+            int value = stateCensusAnalyser.readStateData(CSVStates.class,"");
+            Assert.assertEquals(37, value);
+        } catch (CensusCsvException e) {
+            Assert.assertEquals("Exception due to incorrect delimiter position", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenCsvFile_CheckNumberOfRecodesMatchesOrNot_ForStatesCensus_ShouldReturnTrue() {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("/home/admin265/IdeaProjects/StateCensusAnalyserProblem/src/main/resources/StateCensusData.csv");
+        int count = 0;
+        try {
+            count = stateCensusAnalyser.readStateData(CSVStatesCensus.class,"");
+        } catch (CensusCsvException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(29, count);
+    }
+
+    @Test
+    public void givenCsvFile_ifFoundIncorrectName_OfStatesCensusFile_ShouldThrowException() throws IOException {
+        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("/home/admin265/IdeaProjects/StateCensusAnalyserProblem/src/main/resources/StateCensusData.csv");
+        try {
+            int value = stateCensusAnalyser.readStateData(CSVStatesCensus.class,"");
+            Assert.assertEquals(29, value);
+        } catch (CensusCsvException e) {
+            Assert.assertEquals("Please enter proper file name", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenWrongFilePath_ShouldThrowRunTimeException_ReturnFalse() {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("/home/admin265/IdeaProjects/StateCensusAnalyserProblem/gradlew.bat");
+            int checkNumberOfRecords = stateCensusAnalyser.readStateData(CSVStatesCensus.class,"");
+        } catch (CensusCsvException e) {
+            e.printStackTrace();
+            Assert.assertEquals("Exception due to Header or mismatch data", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenCsvFile_ifFoundIncorrectDelimiterPositionInCSVStatusCensus_ShouldReturnException() throws IOException, CensusCsvException {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("/home/admin265/IdeaProjects/StateCensusAnalyserProblem/src/main/resources/StateCensusData.csv");
+            int value = stateCensusAnalyser.readStateData(CSVStatesCensus.class,"");
+            Assert.assertEquals(29, value);
+        } catch (CensusCsvException e) {
+            Assert.assertEquals("Exception due to Header or mismatch data", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenCsvFile_ifFoundNoHeaderInCSVStatusCensus_ShouldReturnException() {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("/home/admin265/IdeaProjects/StateCensusAnalyserProblem/src/main/resources/StateCensusData.csv");
+            int value = stateCensusAnalyser.readStateData(CSVStatesCensus.class,"");
+            Assert.assertEquals(29, value);
+        } catch (CensusCsvException e) {
+            Assert.assertEquals("Exception due to Header", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenCsvFile_ifSortBystateAlphabeticalOrder_ShouldReturnTrue() {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("/home/admin265/IdeaProjects/StateCensusAnalyserProblem/src/main/resources/StateCensusData.csv");
+            int value = stateCensusAnalyser.readStateData(CSVStatesCensus.class,"stateAlphabeticalOrder");
+            Assert.assertEquals(29, value);
+        } catch (CensusCsvException e) {
+            Assert.assertEquals("Exception due to Header", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenCsvFile_ifSortByMostPopulatedState_ShouldReturnTrue() {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("/home/admin265/IdeaProjects/StateCensusAnalyserProblem/src/main/resources/StateCensusData.csv");
+            int value = stateCensusAnalyser.readStateData(CSVStatesCensus.class,"mostPopulatedState");
+            Assert.assertEquals(29, value);
+        } catch (CensusCsvException e) {
+            Assert.assertEquals("Exception due to Header", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenCsvFile_ifSortByPopulationDensityState_ShouldReturnTrue() {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("/home/admin265/IdeaProjects/StateCensusAnalyserProblem/src/main/resources/StateCensusData.csv");
+            int value = stateCensusAnalyser.readStateData(CSVStatesCensus.class,"populationDensityState");
+            Assert.assertEquals(29, value);
+        } catch (CensusCsvException e) {
+            Assert.assertEquals("Exception due to Header", e.getMessage());
+        }
+    }
+
+    @Test
+    public void givenCsvFile_ifSortByLargestStateArea_ShouldReturnTrue() {
+        try {
+            StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser("/home/admin265/IdeaProjects/StateCensusAnalyserProblem/src/main/resources/StateCensusData.csv");
+            int value = stateCensusAnalyser.readStateData(CSVStatesCensus.class,"largestStateArea");
+            Assert.assertEquals(29, value);
+        } catch (CensusCsvException e) {
+            Assert.assertEquals("Exception due to Header", e.getMessage());
+        }
+    }
+
+}
