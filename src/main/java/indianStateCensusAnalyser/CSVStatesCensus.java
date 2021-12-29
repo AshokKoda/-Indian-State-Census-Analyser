9b7667285e7b47ca8b7362aa418f4d8a022f6f5b
Ashok Koda <a.koda59@gmail.com>
2021-12-29 09:14:32 +0530

    StateCensusData in json file for most population state

---
+++ src/main/java/indianStateCensusAnalyser/CSVStatesCensus.java
@@ -0,0 +1,67 @@
+package indianStateCensusAnalyser;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVStatesCensus {
+
+	public CSVStatesCensus() {
+    }
+
+    @CsvBindByName
+    private String state;
+    @CsvBindByName
+    private String population;
+    @CsvBindByName
+    private String areaInSqKm;
+    @CsvBindByName
+    private String densityPerSqKm;
+
+    public CSVStatesCensus(String state, String population, String areaInSqKm, String densityPerSqKm) {
+        this.state = state;
+        this.population = population;
+        this.areaInSqKm = areaInSqKm;
+        this.densityPerSqKm = densityPerSqKm;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+
+    public String getAreaInSqKm() {
+        return areaInSqKm;
+    }
+
+    public void setAreaInSqKm(String areaInSqKm) {
+        this.areaInSqKm = areaInSqKm;
+    }
+
+    public String getDensityPerSqKm() {
+        return densityPerSqKm;
+    }
+
+    public void setDensityPerSqKm(String densityPerSqKm) {
+        this.densityPerSqKm = densityPerSqKm;
+    }
+
+    @Override
+    public String toString() {
+        return "CSVStatesCensus{" +
+                "state='" + state + '\'' +
+                ", population=" + population +
+                ", areaInSqKm=" + areaInSqKm +
+                ", densityPerSqKm=" + densityPerSqKm +
+                '}';
+    }
+}
