faae784d6029e7fcbd255441e983773cd6764396
Ashok Koda <a.koda59@gmail.com>
2021-12-29 09:08:42 +0530

    StateCensusData in json file In sorted order

---
+++ src/main/java/indianStateCensusAnalyser/CSVStateCensus.java
@@ -0,0 +1,67 @@
+package indianStateCensusAnalyser;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVStateCensus {
+
+	public CSVStateCensus() {
+    }
+
+    @CsvBindByName
+    private String state;
+    @CsvBindByName
+    private Double population;
+    @CsvBindByName
+    private Double areaInSqKm;
+    @CsvBindByName
+    private Double densityPerSqKm;
+
+    public CSVStateCensus(String state, Double population, Double areaInSqKm, Double densityPerSqKm) {
+        this.state = state;
+        this.population = population;
+        this.areaInSqKm = areaInSqKm;
+        this.densityPerSqKm = densityPerSqKm;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Double getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Double population) {
+        this.population = population;
+    }
+
+    public Double getAreaInSqKm() {
+        return areaInSqKm;
+    }
+
+    public void setAreaInSqKm(Double areaInSqKm) {
+        this.areaInSqKm = areaInSqKm;
+    }
+
+    public Double getDensityPerSqKm() {
+        return densityPerSqKm;
+    }
+
+    public void setDensityPerSqKm(Double densityPerSqKm) {
+        this.densityPerSqKm = densityPerSqKm;
+    }
+
+    @Override
+    public String toString() {
+        return "CSVStateCensus{" +
+                "state='" + state + '\'' +
+                ", population=" + population +
+                ", areaInSqKm=" + areaInSqKm +
+                ", densityPerSqKm=" + densityPerSqKm +
+                '}';
+    }
+}
